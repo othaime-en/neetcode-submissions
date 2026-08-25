7a5af01c766326da672a2ad795810d9096350c15
othaime-en <61498231+othaime-en@users.noreply.github.com>
2026-08-25 17:34:04 +0300

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        print("revised nums: ", nums)
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
+        
+        return False
